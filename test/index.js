ea569cd9a08658c5d06f39d61ae85145f91ec74f
Adam Jaso <ajaso@sazze.com>
2016-03-30 22:29:17 -0700

    don't use arrow functions since older projects using older node versions crash

--- test/index.js
+++ test/index.js
@@ -218,7 +218,7 @@ var tests = {
   
   transgress: {
     'should see non-enumerable properties': {
-      args: [(() => {
+      args: [(function() {
         let obj = {};
         obj.enumerable = true;
 
